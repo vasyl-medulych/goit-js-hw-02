0d7d25b51aaf9df08de90c651cdcd962958adfad
Vasyl Linkin <linkinmvv@gmail.com>
2025-06-07 17:03:41 +0300

    hm-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,19 @@
-// Задача 1. Замовлення дроїдів
+console.log('Задача 1. Замовлення дроїдів');
 
-'use strict';
-function makeTransaction(quantity, pricePerDroid) {
+('use strict');
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
   let totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  if (totalPrice <= customerCredits) {
+    return 'Insufficient funds!';
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log('_________________________________________________________');
--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,22 @@
-// Задача 3. Ширина елемента
+console.log('Задача 3. Ширина елемента');
 
-'use strict';
-function getElementWidth(content, padding, border) {
-  const totalWidth =
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-  return totalWidth;
+('use strict');
+function checkForSpam(message) {
+  const isSpam = message.toLowerCase().includes('spam');
+  const isSale = message.toLowerCase().includes('sale');
+  if (isSpam || isSale) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+console.log('________________________________________________________');
